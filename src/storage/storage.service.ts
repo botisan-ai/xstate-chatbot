2078031e460c79f4a6a820bcbd2b934214437b12
Simon Liang <no.lhr0909@gmail.com>
2022-01-08 18:43:59 +0800

    changed to use redis stream as state storage so we get full history

--- src/storage/storage.service.ts
+++ src/storage/storage.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { RedisService } from '@liaoliaots/nestjs-redis';
 import { Redis } from 'ioredis';
 import { State } from 'xstate';
@@ -7,6 +7,7 @@ const PREFIX = 'state';
 
 @Injectable()
 export class StorageService {
+  private logger = new Logger(StorageService.name);
   private client: Redis;
 
   constructor(private readonly redisService: RedisService) {
@@ -17,14 +18,37 @@ export class StorageService {
     sender: string,
     value: State<any, any, any, any>,
   ): Promise<void> {
-    await this.client.set(`${PREFIX}:${sender}`, JSON.stringify(value));
+    await this.client.xadd(
+      `${PREFIX}:${sender}`,
+      '*',
+      'state',
+      JSON.stringify(value),
+    );
   }
 
   public async fetch(
     sender: string,
     defaultState?: State<any, any, any, any>,
   ): Promise<State<any, any, any, any> | undefined> {
-    const state = await this.client.get(`${PREFIX}:${sender}`);
-    return state ? State.create(JSON.parse(state)) : defaultState;
+    const item = await this.client.xrange(
+      `${PREFIX}:${sender}`,
+      '-',
+      '+',
+      'COUNT',
+      1,
+    );
+
+    if (item.length === 0) {
+      return defaultState;
+    }
+
+    const [, state] = item[0][1];
+    try {
+      return state ? State.create(JSON.parse(state)) : defaultState;
+    } catch (err) {
+      this.logger.error(`error getting state: ${err.message}`);
+      console.error(err);
+      return defaultState;
+    }
   }
 }
